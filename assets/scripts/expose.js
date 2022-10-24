83d07b56968072becf6761f4095fcc9e8f6b8c4b
hugompham <hmpham@ucsd.edu>
2022-10-24 16:42:32 -0700

    Testing whether select horns change image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  document.getElementById('horn-select').addEventListener('change', (event) => {
+  const imageHorn = document.querySelector("[type='img']");
+  switch(event.target.value){
+    case "air-horn":
+      imageHorn.src = "air-horn.svg";
+      break;
+    case "car-horn":
+      imageHorn.src = "car-horn.svg";
+      break;
+    case "party-horn":
+      imageHorn.src = "party-horn.svg";
+      break;
+  }
+  });
 }
