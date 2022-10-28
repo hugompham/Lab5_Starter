2c431807e83e4b5b996bf0fd904839492b6d32d6
Tung <tan002@ucsd.edu>
2022-10-28 16:20:20 -0700

    Finishing up explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -10,6 +10,17 @@ const voiceSelect = document.querySelector('select');
 // Grab the speechSynthesis API
 const synth = window.speechSynthesis; 
 
+// Grab a reference to the 'Press to Talk' button
+const buttonClick = document.querySelector('button');
+
+// Grab a reference to the textarea element
+const textArea = document.getElementById('text-to-speak');
+
+// Grab a reference to the smiling face
+const smileFace = document.querySelector('img');
+
+// Function to populate the drop down list with voices
+// available in the browser
 function populateVoices() {
   voices = synth.getVoices(); // Grab all voices and store to the array
 
@@ -35,30 +46,50 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoices;
 }
 
-// Grab a reference to the 'Press to Talk' button
-buttonClick = document.querySelector('button');
-
-// Grab a reference to the textarea element
-textArea = document.getElementById('text-to-speak');
-
 /* Upon finished loading */
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
+function init() { 
+  // Behavior: When there is an ongoing speech and the user clicks the button again, 
+  // the new speech will be added to the queue, waiting for the ongoing speech 
+  // to finish first.
   buttonClick.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    
+    // Create a new speech synthesizer object for the texts from the textbox
     const utterThis = new SpeechSynthesisUtterance(textArea.value);
+
+    // Grab the selected voice from the drop down list
     const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
 
+    // Loop over the voices array to compare and see which voice
+    // the user selected. Then assign said voice to the object's 
+    // voice property.
     for (let i = 0; i < voices.length ; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
       }
     }
     
+    // We don't have dedicate sliders/buttons for these values, 
+    // so we default them to 1
     utterThis.pitch = 1;
     utterThis.volume = 1;
     utterThis.rate = 1;
 
+    // Queue the new speech
     synth.speak(utterThis);
-  });
+    
+    // Upon starting to speak, the smiling face will open its mouth
+    utterThis.addEventListener('start', () => {
+      smileFace.src = "assets/images/smiling-open.png";
+    });
+
+    // Once the speech is finished, reset back to smiling face
+    // Bug: Up to 1 sec delay (depending on the voice) for the face
+    // to reset back to smiling
+    utterThis.addEventListener('end', () => {
+      smileFace.src = "assets/images/smiling.png";
+    });
+  })
 }
