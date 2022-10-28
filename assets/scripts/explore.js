47432e098c0ab2d771787924fb4a6f803074bb03
Tung <tan002@ucsd.edu>
2022-10-28 13:46:42 -0700

    First Draft

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,64 @@
 // explore.js
 
+/* Declaring some important variables first */
+// Array to store all voices 
+let voices = []; 
+
+// Grab a reference to the 'select' element
+const voiceSelect = document.querySelector('select'); 
+
+// Grab the speechSynthesis API
+const synth = window.speechSynthesis; 
+
+function populateVoices() {
+  voices = synth.getVoices(); // Grab all voices and store to the array
+
+  // For each voice in the array, we make an "option" out of it
+  // then append to the 'select' element in explore.js
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoices(); // We load all available voices from the API
+ 
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+// Grab a reference to the 'Press to Talk' button
+buttonClick = document.querySelector('button');
+
+// Grab a reference to the textarea element
+textArea = document.getElementById('text-to-speak');
+
+/* Upon finished loading */
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  buttonClick.addEventListener('click', (event) => {
+    const utterThis = new SpeechSynthesisUtterance(textArea.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    
+    utterThis.pitch = 1;
+    utterThis.volume = 1;
+    utterThis.rate = 1;
+
+    synth.speak(utterThis);
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,21 +5,21 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   document.getElementById('horn-select').addEventListener('change', (event) => {
-  const imageHorn = document.querySelector("img");
-  const soundHorn = document.querySelector("audio")
-  switch(event.target.value){
-    case "air-horn":
-      imageHorn.src = "assets/images/air-horn.svg";
-      soundHorn.src = "assets/audio/air-horn.mp3";
-      break;
-    case "car-horn":
-      imageHorn.src = "assets/images/car-horn.svg";
-      soundHorn.src = "assets/audio/car-horn.mp3";
-      break;
-    case "party-horn":
-      imageHorn.src = "assets/images/party-horn.svg";
-      soundHorn.src = "assets/audio/party-horn.mp3";
-      break;
-  }
+    const imageHorn = document.querySelector("img");
+    const soundHorn = document.querySelector("audio")
+    switch(event.target.value){
+      case "air-horn":
+        imageHorn.src = "assets/images/air-horn.svg";
+        soundHorn.src = "assets/audio/air-horn.mp3";
+        break;
+      case "car-horn":
+        imageHorn.src = "assets/images/car-horn.svg";
+        soundHorn.src = "assets/audio/car-horn.mp3";
+        break;
+      case "party-horn":
+        imageHorn.src = "assets/images/party-horn.svg";
+        soundHorn.src = "assets/audio/party-horn.mp3";
+        break;
+    }
   });
 }
